ef8d7297ba8b75530d97c0cf5a36f320304024d6
Sima-Shulman <Hand4d31@mbjcomp.org.il>
2024-10-29 00:10:06 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program8371.cs
@@ -0,0 +1,20 @@
+﻿namespace stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome8371();
+            Welcome0429();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0429();
+        private static void Welcome8371()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"{userName}, welcome to my first console application");
+        }
+    }
+}
