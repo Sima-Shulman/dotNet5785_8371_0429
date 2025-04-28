ea223b4019f4a9349d455808238dde0ef5b8be82
Sima-Shulman <Hand4d31@mbjcomp.org.il>
2025-04-28 19:09:05 +0300

    Stage 5  entire Chapter 6

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using PL.Call;
+using PL.Volunteer;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,88 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+
+        public TimeSpan RiskRange
+        {
+            get { return (TimeSpan)GetValue(RiskRangeProperty); }
+            set { SetValue(RiskRangeProperty, value); }
+        }
+
+        public static readonly DependencyProperty RiskRangeProperty =
+            DependencyProperty.Register("RiskRange", typeof(TimeSpan), typeof(MainWindow));
+
+        private void MainWindow_Loaded(object sender,EventArgs e)
+        {
+            // Initialize the current time and risk range
+            CurrentTime = s_bl.Admin.GetClock();
+            RiskRange = s_bl.Admin.GetRiskTimeRange();
+            s_bl.Admin.AddClockObserver(clockObserver);
+            s_bl.Admin.AddConfigObserver(configObserver);
+        }
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            s_bl.Admin.RemoveClockObserver(clockObserver);
+            s_bl.Admin.RemoveConfigObserver(configObserver);
+        }
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.PromoteClock(BO.Enums.TimeUnit.Minute);
+        }
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.PromoteClock(BO.Enums.TimeUnit.Hour);
+        }
+        private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.PromoteClock(BO.Enums.TimeUnit.Day);
+        }
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.PromoteClock(BO.Enums.TimeUnit.Year);
+        }
+        private void btnUpdateRiskRange_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.SetRiskTimeRange(RiskRange);
+        }
+        private void btnHandleVolunteers_Click(object sender, RoutedEventArgs e)
+        {
+            new VolunteerListWindow().Show();
+        }
+        private void btnHandleCalls_Click(object sender, RoutedEventArgs e)
+        {
+           new CallListWindow().Show();
+        }
+        private void btnInitDB_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.InitializeDatabase();
+        }
+        private void btnResetDB_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ResetDatabase();
+        }
+        private void clockObserver()
+        {
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+        private void configObserver()
+        {
+            RiskRange = s_bl.Admin.GetRiskTimeRange();
         }
     }
 }
